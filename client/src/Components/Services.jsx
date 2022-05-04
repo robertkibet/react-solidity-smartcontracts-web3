a4ae2eeb271028069d642ed47970c2db68bf8d32
Robert Kibet <kipronokrobert@gmail.com>
2022-05-04 16:23:06 +0300

    basic services screen

--- client/src/Components/Services.jsx
+++ client/src/Components/Services.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import {BsShieldFillCheck} from "react-icons/bs";
+import {BiSearchAlt} from "react-icons/bi";
+import {RiHeart2Fill} from "react-icons/ri";
 
+const ServiceCard = ({color, title, icons, subtitle}) => {
+  return (
+    <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
+      <div
+        className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}
+      >
+        {icons}
+      </div>
+      <div className="ml-5 flex flex-col flex-1">
+        <h1 className="mt-2 text-white text-lg">{title}</h1>
+        <p className="text-white text-sm md:w-9/12">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="flex flex-col md:flex-row w-full justify-center items-center gradient-bg-services">
+      <div className="flex w-full md:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 flex-col justify-start items-center">
+          <h1 className="text-white text-3xl sm:text-5xl py-2 text-gradient">
+            Services that we
+            <br /> continue to improve
+          </h1>
+        </div>
+        <div className="flex-1 flex flex-col justify-start items-center">
+          <ServiceCard
+            color="bg-[#2952e3]"
+            title="Security Guaranteed"
+            icons={<BsShieldFillCheck className="text-white" fontSize={21} />}
+            subtitle="We are always on the lookout for the best security"
+          />
+          <ServiceCard
+            color="bg-[#8945F8]"
+            title="Best exchange rates"
+            icons={<BiSearchAlt className="text-white" fontSize={21} />}
+            subtitle="We are always on the lookout for the best security"
+          />
+          <ServiceCard
+            color="bg-[#f84550]"
+            title="Fastest transactions"
+            icons={<BsShieldFillCheck className="text-white" fontSize={21} />}
+            subtitle="We are always on the lookout for the best security"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
