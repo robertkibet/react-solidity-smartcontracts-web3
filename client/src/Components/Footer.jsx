c3d5df8b0d5c05efe9d7fc90c4f628afb3cd5e96
Robert Kibet <kipronokrobert@gmail.com>
2022-05-04 17:03:30 +0300

    simple footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import logo from "../../images/logo.png";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full flex md: justify-center justify-between flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row justify-between items-centermy-4">
+        <div className="flex flex-[0.5] justify-center items-center">
+          <img src={logo} alt="logo" className="w-32" />
+        </div>
+        <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Markets
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Careers
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Docs
+          </p>
+          <p className="text-white text-base text-center mx-2 cursor-pointer">
+            Wallets
+          </p>
+        </div>
+      </div>
+      Footer
+    </div>
+  );
 };
 
 export default Footer;
